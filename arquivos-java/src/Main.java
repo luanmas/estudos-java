3b2c18aeb83c757a0578042862cae57ee6172ff3
luanmas <luanmas27@gmail.com>
2025-05-20 22:10:17 -0300

    arquivos-java: aprendendo a ler arquvios com fileReader e BufferReader

--- arquivos-java/src/Main.java
+++ arquivos-java/src/Main.java
@@ -1,23 +1,59 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("C:\\Windows\\Temp\\teste-para-java.txt");
-        Scanner sc = null;
+        // Leitura de arquivo usando File e Scanner
+//        File file = new File("C:\\Windows\\Temp\\teste-para-java.txt");
+//        Scanner sc = null;
+//
+//        try {
+//            sc = new Scanner(file);
+//            while (sc.hasNextLine()) {
+//                System.out.println(sc.nextLine());
+//            }
+//        } catch (IOException e) {
+//            System.out.println("Erro " + e.getMessage());
+//        } finally {
+//            if (sc != null) {
+//                sc.close();
+//            }
+//        }
+
+
+        // Leitura de arquivo usando FileReader e BufferedReader
+        String path = "C:\\Windows\\Temp\\teste-para-java.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
 
         try {
-            sc = new Scanner(file);
-            while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
             }
         } catch (IOException e) {
             System.out.println("Erro " + e.getMessage());
         } finally {
-            if (sc != null) {
-                sc.close();
+            try {
+                if (fr != null) {
+                    fr.close();
+                }
+
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
+
+
     }
 }
