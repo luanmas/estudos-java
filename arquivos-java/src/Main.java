de7f87ec196e3d27244af02a316735f5c1728f73
luanmas <luanmas27@gmail.com>
2025-05-20 21:56:42 -0300

    arquivos-java: primeira maneira para leitura de arquivos

---
+++ arquivos-java/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("C:\\Windows\\Temp\\teste-para-java.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Erro " + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
+}
